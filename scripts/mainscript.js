ec168f9d27e3689d47125c3d769af80b911d24bf
Christoffer Sommerfeld <35817456+ChristoffSomm@users.noreply.github.com>
2019-02-19 10:55:09 -0700

    Added things

---
+++ scripts/mainscript.js
@@ -0,0 +1,62 @@
+function displayVideo1() {
+	document.getElementById("video1").style.display = "block";
+	document.getElementById("video2").style.display = "none";
+	document.getElementById("video3").style.display = "none";
+}
+
+function displayVideo2() {
+	document.getElementById("video1").style.display = "none";
+	document.getElementById("video2").style.display = "block";
+	document.getElementById("video3").style.display = "none";
+}
+
+function displayVideo3() {
+	document.getElementById("video1").style.display = "none";
+	document.getElementById("video2").style.display = "none";
+	document.getElementById("video3").style.display = "block";
+}
+
+function displayVideo4() {
+	document.getElementById("video4").style.display = "block";
+	document.getElementById("video5").style.display = "none";
+	document.getElementById("video6").style.display = "none";
+}
+
+function displayVideo5() {
+	document.getElementById("video4").style.display = "none";
+	document.getElementById("video5").style.display = "block";
+	document.getElementById("video6").style.display = "none";
+}
+
+function displayVideo6() {
+	document.getElementById("video4").style.display = "none";
+	document.getElementById("video5").style.display = "none";
+	document.getElementById("video6").style.display = "block";
+}
+
+function displayVideo7() {
+	document.getElementById("video7").style.display = "block";
+	document.getElementById("video8").style.display = "none";
+	document.getElementById("video9").style.display = "none";
+}
+
+function displayVideo8() {
+	document.getElementById("video7").style.display = "none";
+	document.getElementById("video8").style.display = "block";
+	document.getElementById("video9").style.display = "none";
+}
+function displayVideo9() {
+	document.getElementById("video7").style.display = "none";
+	document.getElementById("video8").style.display = "none";
+	document.getElementById("video9").style.display = "block";
+}
+
+function displayVideo10() {
+	document.getElementById("video10").style.display = "block";
+	document.getElementById("video11").style.display = "none";
+}
+
+function displayVideo11 () {
+	document.getElementById("video10").style.display = "none";
+	document.getElementById("video11").style.display = "block";
+}
