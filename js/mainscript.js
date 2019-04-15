4c1b9878d8e2409acbf70153057825af58262bd6
Christoffer Sommerfeld <35817456+ChristoffSomm@users.noreply.github.com>
2019-04-15 09:45:37 -0600

    Site2.0

---
+++ js/mainscript.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+	
+	//northen herd
+  $('#flip-btn').click(function() {
+    $('.box_holder').toggleClass('show_info');
+  });
+	$('#unflip-btn').click(function() {
+    $('.box_holder').toggleClass('show_info');
+  });
+	
+	//Guy hunt
+	$('#flip-btn1').click(function() {
+    $('.box_holder1').toggleClass('show_info1');
+  });
+	$('#unflip-btn1').click(function() {
+    $('.box_holder1').toggleClass('show_info1');
+  });
+	
+	//Dad hunt
+	$('#flip-btn2').click(function() {
+    $('.box_holder2').toggleClass('show_info2');
+  });
+	$('#unflip-btn2').click(function() {
+    $('.box_holder2').toggleClass('show_info2');
+  });
+	
+	//New Blood
+	$('#flip-btn_R1V1').click(function() {
+    $('.box_holder_R1V1').toggleClass('show_info_R1V1');
+  });
+	$('#unflip-btn_R1V1').click(function() {
+    $('.box_holder_R1V1').toggleClass('show_info_R1V1');
+  });
+	
+	//Rewarming Drill
+	$('#flip-btn_R1V2').click(function() {
+    $('.box_holder_R1V2').toggleClass('show_info_R1V2');
+  });
+	$('#unflip-btn_R1V2').click(function() {
+    $('.box_holder_R1V2').toggleClass('show_info_R1V2');
+  });
+	
+	//The Best Bowfishing
+	$('#flip-btn_R1V3').click(function() {
+    $('.box_holder_R1V3').toggleClass('show_info_R1V3');
+  });
+	$('#unflip-btn_R1V3').click(function() {
+    $('.box_holder_R1V3').toggleClass('show_info_R1V3');
+  });
+	
+		//Lowa Z-8S GTX Boot
+	$('#flip-btn_R2V1').click(function() {
+    $('.box_holder_R2V1').toggleClass('show_info_R2V1');
+  });
+	$('#unflip-btn_R2V1').click(function() {
+    $('.box_holder_R2V1').toggleClass('show_info_R2V1');
+  });
+	
+	//Sitka Delta Waders
+	$('#flip-btn_R2V2').click(function() {
+    $('.box_holder_R2V2').toggleClass('show_info_R2V2');
+  });
+	$('#unflip-btn_R2V2').click(function() {
+    $('.box_holder_R2V2').toggleClass('show_info_R2V2');
+  });
+	
+	//Shooting Stability
+	$('#flip-btn_R2V3').click(function() {
+    $('.box_holder_R2V3').toggleClass('show_info_R2V3');
+  });
+	$('#unflip-btn_R2V3').click(function() {
+    $('.box_holder_R2V3').toggleClass('show_info_R2V3');
+  });
+	
+		//Decoy Weights
+	$('#flip-btn_R3V1').click(function() {
+    $('.box_holder_R3V1').toggleClass('show_info_R3V1');
+  });
+	$('#unflip-btn_R3V1').click(function() {
+    $('.box_holder_R3V1').toggleClass('show_info_R3V1');
+  });
+	
+	//Remove Skunk Smell
+	$('#flip-btn_R3V2').click(function() {
+    $('.box_holder_R3V2').toggleClass('show_info_R3V2');
+  });
+	$('#unflip-btn_R3V2').click(function() {
+    $('.box_holder_R3V2').toggleClass('show_info_R3V2');
+  });
+	
+	//RaptoRazor
+	$('#flip-btn_R3V3').click(function() {
+    $('.box_holder_R3V3').toggleClass('show_info_R3V3');
+  });
+	$('#unflip-btn_R3V3').click(function() {
+    $('.box_holder_R3V3').toggleClass('show_info_R3V3');
+  });
+	
+$(".facebook").click(function() {
+window.location = "https://www.facebook.com/chris.sommerfeld.5";
+});
+	
+$(".youtube").click(function() {
+window.location = "https://www.youtube.com/channel/UCXiy278p1to6SX4qFDwpZhQ?view_as=subscriber";
+});
+		
+	
+});
+
+
+
+
